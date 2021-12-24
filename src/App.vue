e466e8c6dea357e8603b944c4e3ff7e5b70682b0
elissa_liu <524948583@qq.com>
2021-12-24 11:26:40 +0800

    perf: 全局中文

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,28 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <router-view/>
+    <a-config-provider :locale="locale">
+      <router-view/>
+    </a-config-provider>
   </div>
 </template>
+
+<script>
+import { ConfigProvider } from 'ant-design-vue';
+import zhCN from 'ant-design-vue/es/locale/zh_CN.js';
+
+export default {
+  name: "app",
+  components: {
+    AConfigProvider:ConfigProvider
+  },
+  data() {
+    return {
+      locale: zhCN
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 
